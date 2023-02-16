9407dbd28f8dd2faada642019ef92b9ddd1294a9
highergear <ndrew.sebastian@gmail.com>
2023-02-16 13:08:54 +0700

    add Format Function when writing table to BQ

--- src/main/java/com/highergear/beam/sample/AWSToBQETLSimple.java
+++ src/main/java/com/highergear/beam/sample/AWSToBQETLSimple.java
@@ -1,5 +1,8 @@
 package com.highergear.beam.sample;
 
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.api.services.bigquery.model.TableSchema;
 import com.highergear.beam.sample.model.Customer;
 import com.highergear.beam.sample.options.AWSToBQETLOptions;
 import com.highergear.beam.sample.schema.CustomerTableSchema;
@@ -9,6 +12,8 @@ import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.ParDo;
+
+import java.util.List;
 
 import static com.highergear.beam.sample.options.AwsOptionsParser.formatOptions;
 
@@ -23,6 +28,10 @@ public class AWSToBQETLSimple {
 
         Pipeline p = Pipeline.create(options);
 
+        TableSchema schema = CustomerTableSchema.Create();
+        List<TableFieldSchema> schemaFields = schema.getFields();
+
+
         p.apply("Read Customer",
                         TextIO.read().from(options.getInputBucketUrl() + "customer.csv"))
                 .apply("Transform Line", ParDo.of(new MapToCustomer()))
@@ -30,7 +39,26 @@ public class AWSToBQETLSimple {
                         BigQueryIO
                                 .<Customer>write()
                                 .to(options.getBigQueryTableName())
-                                .withSchema(CustomerTableSchema.Create()));
+                                .withSchema(schema)
+                                .withFormatFunction((Customer customer) ->
+                                        new TableRow()
+                                                .set(schemaFields.get(0).getName(), customer.getCustomerId())
+                                                .set(schemaFields.get(1).getName(), customer.isNameStyle())
+                                                .set(schemaFields.get(2).getName(), customer.getTitle())
+                                                .set(schemaFields.get(3).getName(), customer.getFirstName())
+                                                .set(schemaFields.get(4).getName(), customer.getMiddleName())
+                                                .set(schemaFields.get(5).getName(), customer.getLastName())
+                                                .set(schemaFields.get(6).getName(), customer.getSuffix())
+                                                .set(schemaFields.get(7).getName(), customer.getCompanyName())
+                                                .set(schemaFields.get(8).getName(), customer.getSalesPerson())
+                                                .set(schemaFields.get(9).getName(), customer.getEmailAddress())
+                                                .set(schemaFields.get(10).getName(), customer.getPhone())
+                                                .set(schemaFields.get(11).getName(), customer.getPasswordHash())
+                                                .set(schemaFields.get(12).getName(), customer.getPasswordSalt())
+                                                .set(schemaFields.get(13).getName(), customer.getRowGuid())
+                                                .set(schemaFields.get(14).getName(), customer.getModifiedDate()))
+                                .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+                                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
 
         p.run().waitUntilFinish();
     }
